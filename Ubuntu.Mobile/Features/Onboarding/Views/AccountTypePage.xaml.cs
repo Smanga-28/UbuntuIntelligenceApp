008d4ec9bb2c8b12e9dc437999e893804927fe1d
Nqobile Mlando Mngadi <ndoshbean18@gmail.com>
2026-09-14 12:35:45 +0200

    Added account type page

---
+++ Ubuntu.Mobile/Features/Onboarding/Views/AccountTypePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace UbuntuIntelligenceApp.Ubuntu.Mobile.Features.Onboarding.Views;
+
+public partial class AccountTypePage : ContentPage
+{
+	public AccountTypePage()
+	{
+		InitializeComponent();
+	}
+}
