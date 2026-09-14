48422e525054ec8ec667cdf659db569572ff3904
smangaliso Nkanyezi Ntshangase <150956284+Smanga-28@users.noreply.github.com>
2026-09-14 12:58:39 +0200

    Created an Ubuntu Bank solution

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace UbuntuIntelligenceApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
