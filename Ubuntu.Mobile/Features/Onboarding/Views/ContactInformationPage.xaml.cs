458e0ef892059e1263d9cc8f27e88d8cade628b5
smangaliso Nkanyezi Ntshangase <150956284+Smanga-28@users.noreply.github.com>
2026-09-14 14:34:37 +0200

    Added new file under Onboarding views

---
+++ Ubuntu.Mobile/Features/Onboarding/Views/ContactInformationPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace UbuntuIntelligenceApp.Ubuntu.Mobile.Features.Onboarding.Views;
+
+public partial class ContactInformationPage : ContentPage
+{
+	public ContactInformationPage()
+	{
+		InitializeComponent();
+	}
+}
