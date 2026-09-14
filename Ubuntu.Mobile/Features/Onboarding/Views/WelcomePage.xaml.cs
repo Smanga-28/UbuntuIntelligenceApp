da4a13ae954a4f8242059c06e67203add1a5eb79
smangaliso Nkanyezi Ntshangase <150956284+Smanga-28@users.noreply.github.com>
2026-09-14 13:30:42 +0200

    Added new empty files

---
+++ Ubuntu.Mobile/Features/Onboarding/Views/WelcomePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace UbuntuIntelligenceApp.Ubuntu.Mobile.Features.Onboarding.Views;
+
+public partial class WelcomePage : ContentPage
+{
+	public WelcomePage()
+	{
+		InitializeComponent();
+	}
+}
