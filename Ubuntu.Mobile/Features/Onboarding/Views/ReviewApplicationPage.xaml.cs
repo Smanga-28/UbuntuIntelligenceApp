0b6d58d5c15e738797aceeb037c0e05b81177fe7
lilithabokwe-prog <lilitha.bokwe@gmail.com>
2026-09-15 15:30:24 +0200

    Added ReviewApplicationPage

---
+++ Ubuntu.Mobile/Features/Onboarding/Views/ReviewApplicationPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace UbuntuIntelligenceApp.Ubuntu.Mobile.Features.Onboarding.Views;
+
+public partial class ReviewApplicationPage : ContentPage
+{
+	public ReviewApplicationPage()
+	{
+		InitializeComponent();
+	}
+}
